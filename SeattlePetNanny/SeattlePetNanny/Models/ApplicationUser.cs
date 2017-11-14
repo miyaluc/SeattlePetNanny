fc797b45953debf645b23e214cd4746bdc48a571
Adam <awalls1@gmail.com>
2017-11-14 10:03:53 -0800

    commented out Birthday

--- SeattlePetNanny/SeattlePetNanny/Models/ApplicationUser.cs
+++ SeattlePetNanny/SeattlePetNanny/Models/ApplicationUser.cs
@@ -9,8 +9,8 @@ namespace SeattlePetNanny.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        [DataType(DataType.Date)]
-        public DateTime Birthday { get; set; }
+        //[DataType(DataType.Date)]
+        //public DateTime Birthday { get; set; }
 
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
