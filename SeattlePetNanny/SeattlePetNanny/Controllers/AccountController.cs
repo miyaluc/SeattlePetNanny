582a18549a059b4859540fe07de933ec5b818ea4
Adam <awalls1@gmail.com>
2017-11-14 14:11:47 -0800

    adding an owner claim to each user at registration

--- SeattlePetNanny/SeattlePetNanny/Controllers/AccountController.cs
+++ SeattlePetNanny/SeattlePetNanny/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using SeattlePetNanny.Models;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 
@@ -17,6 +19,7 @@ namespace SeattlePetNanny.Controllers
             _signInManager = signInManager;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult Login()
         {
@@ -24,6 +27,7 @@ namespace SeattlePetNanny.Controllers
 
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel lvm)
         {
@@ -39,12 +43,14 @@ namespace SeattlePetNanny.Controllers
             return View();
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public IActionResult Register()
         {
             return View();
         }
 
+        [AllowAnonymous]
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel rvm)
         {
@@ -55,17 +61,23 @@ namespace SeattlePetNanny.Controllers
 
                 if (result.Succeeded)
                 {
-                    // adding a claim to this user
-                    var addRole = await _userManager.AddClaimAsync(user, (new Claim(ClaimTypes.Role, "Administrator", ClaimValueTypes.String)));
+                    // adding an Owner claim to each registered user
+                    // user will need the Owner claim to access thier profile
+                    List<Claim> Claims = new List<Claim>();
+                    Claim accountTypeClaim = new Claim(ClaimTypes.Role, "Owner", ClaimValueTypes.String);
+                    Claims.Add(accountTypeClaim);
 
-                    if (addRole.Succeeded)
+                    var addClaims = await _userManager.AddClaimsAsync(user, Claims);
+
+                    //var addRole = await _userManager.AddClaimAsync(user, (new Claim(ClaimTypes.Role, "Administrator", ClaimValueTypes.String)));
+
+                    if (addClaims.Succeeded)
                     {
                         // AWAIT and see whether user was successfully registered
-                        await _signInManager.PasswordSignInAsync(rvm.Email, rvm.Password, true, lockoutOnFailure: false);
+                        await _signInManager.PasswordSignInAsync(rvm.Email, rvm.Password, false, lockoutOnFailure: false);
                         return RedirectToAction("Index", "Home");
                     }
                 }
-
             }
             return View();
         }
