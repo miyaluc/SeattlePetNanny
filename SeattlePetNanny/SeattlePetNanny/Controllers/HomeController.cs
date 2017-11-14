0a2cb7012b219819dbb4c88267211181635adb5f
George Gaylor <georgegaylor83@outlook.com>
2017-11-14 14:01:59 -0800

    added rates page and libs to gitignore

--- SeattlePetNanny/SeattlePetNanny/Controllers/HomeController.cs
+++ SeattlePetNanny/SeattlePetNanny/Controllers/HomeController.cs
@@ -1,16 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SeattlePetNanny.Controllers
 {
-    public class HomeController :  Controller
+    public class HomeController : Controller
     {
-            public IActionResult Index()
-            {
-                return View();
-            }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Rates()
+        {
+            return View();
+        }
     }
 }
