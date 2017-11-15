b6a45b8da951d331c7cb02a4dbfb39c8fb44a658
George Gaylor <georgegaylor83@outlook.com>
2017-11-15 10:41:21 -0800

    Fixed Secondary Owner table add

--- SeattlePetNanny/SeattlePetNanny/Data/SeattlePetNannyContext.cs
+++ SeattlePetNanny/SeattlePetNanny/Data/SeattlePetNannyContext.cs
@@ -1,28 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SeattlePetNanny.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SeattlePetNanny.Data
 {
     public class SeattlePetNannyContext : DbContext
     {
-        public SeattlePetNannyContext(DbContextOptions<SeattlePetNannyContext> options) : base(options)
-        {
-
-        }
+        public SeattlePetNannyContext(DbContextOptions<SeattlePetNannyContext> options) : base(options){ }
 
         public DbSet<Owner> Owner { get; set; }
-
         public DbSet<Dog> Dog { get; set; }
-
         public DbSet<Worker> Worker { get; set; }
-
         public DbSet<Admin> Admin { get; set; }
-
         public DbSet<ReportCard> ReportCard { get; set; }
-
     }
 }
