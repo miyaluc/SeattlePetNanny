bd46cf12ef68da253a87e532c46babaf985c0fbb
George Gaylor <georgegaylor83@outlook.com>
2017-11-15 14:19:58 -0800

    Added Admin controller

---
+++ SeattlePetNanny/SeattlePetNanny/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SeattlePetNanny.Data;
+using SeattlePetNanny.Models;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SeattlePetNanny.Controllers
+{
+    public class AdminController : Controller
+    {
+    }
+}
